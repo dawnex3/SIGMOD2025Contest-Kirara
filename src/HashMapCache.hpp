a687e95ed7dd8cbfab4ecb08ab212ce96cc51ad1
dawnex3 <dawnex@163.com>
2025-03-28 14:22:00 +0000

    实现哈希表缓存。

--- src/HashMapCache.hpp
+++ src/HashMapCache.hpp
@@ -2,6 +2,9 @@
 #include "plan.h"
 #include "HashMap.hpp"
 #include "DataStructure.hpp"
+
+//#define CACHE_LOG
+
 namespace Contest {
 
 // Boost库的hash_combine实现
@@ -51,7 +54,7 @@ public:
         std::vector<uint64_t>   samples_;   // 对列的采样
         uint64_t                hash_;      // 哈希值
 
-        static const size_t SAMPLE_SIZE = 20;    // 采样数目（最大）。实际采样数目是2倍SAMPLE_SIZE
+        inline static const size_t SAMPLE_SIZE = 20;    // 采样数目（最大）。实际采样数目是2倍SAMPLE_SIZE
 
         // 采样一个列，生成哈希值。如果这个列是VARCHAR类型，不用采样了，哈希值设为无效。
         ColumnSample(const Column& column, size_t row_num) : type_(column.type), page_num_(column.pages.size()){
@@ -83,7 +86,7 @@ public:
                 }
 
                 double row_step = static_cast<double>(row_num - 1) / (sample_size - 1);
-                std::vector<uint32_t> sample_row_idx(2*sample_size,0);
+                std::vector<uint32_t> sample_row_idx(2*sample_size+1,0);
                 for(size_t i=0; i<sample_size; i++){
                     sample_row_idx[2*i] = i==0 ? 0 : static_cast<size_t>(std::round(i * row_step)) - 1;
                     sample_row_idx[2*i + 1] = static_cast<size_t>(std::round(i * row_step));
@@ -117,11 +120,11 @@ public:
         std::map<size_t,ColumnSample>   column_samples_;
     };
 
-    // DONT_CACHE: 不要缓存。可能该节点是探测测，或者该节点是构建侧但是输出包含VARCAHR，或者深度过深，或者被释放掉了
-    // NEED_CACHE: 需要缓存。表明该节点希望得到缓存。经过查找缓存和实际执行后，变为HAS_CACHE或者OWN_CACHE
-    // HAS_CACHE: 找到了缓存。此时对应的hashmaps_中存储的是被缓存的哈希表
+    // DONT_CACHE: 不要缓存。可能该节点是scan，或者该join的构建侧包含VARCAHR，或者深度过深，或者被释放掉了
+    // NEED_CACHE: 需要缓存。表明该节点希望得到缓存。经过查找缓存和实际执行后，变为USE_CACHE或者OWN_CACHE
+    // USE_CACHE: 使用缓存。此时对应的hashmaps_中存储的是被缓存的哈希表
     // OWN_CACHE: 拥有缓存。此时对应的hashmaps_中存储的是该语句执行中生成的哈希表
-    enum CacheType{DONT_CACHE, NEED_CACHE, HAS_CACHE, OWN_CACHE};
+    enum CacheType{DONT_CACHE, NEED_CACHE, USE_CACHE, OWN_CACHE};
 
     QueryCache(const Plan& plan)
     : nodes_(plan.nodes), inputs_sample_(plan.inputs.size()), root(plan.root), hashes_(plan.nodes.size(),INVALID_HASH),
@@ -133,23 +136,82 @@ public:
         calculateNodeHash(plan,plan.root);
     }
 
+    void print() const{
+        // 打印节点得到哈希值和类型
+        for(size_t i=0; i<nodes_.size(); i++){
+            if(isJoinNode(i)){
+                printf("Join %zu ", i);
+            } else {
+                printf("Scan %zu ", i);
+            }
+            if(cache_types_[i]==DONT_CACHE){
+                printf("hash %lu DONT_CACHE\n", hashes_[i]);
+            } else if(cache_types_[i]==NEED_CACHE){
+                printf("hash %lu NEED_CACHE\n", hashes_[i]);
+            } else if(cache_types_[i]==USE_CACHE){
+                printf("hash %lu USE_CACHE\n", hashes_[i]);
+            } else {
+                printf("hash %lu OWN_CACHE\n", hashes_[i]);
+            }
+        }
+
+        // 打印基表采样结果
+        for(size_t i=0; i<inputs_sample_.size(); i++){
+            const TableSample& table_sample = inputs_sample_[i];
+            for(const auto& [col_id, col_sample]:table_sample.column_samples_){
+                if(col_sample.type_==DataType::INT32){
+                    printf("table %zu int col %lu hash %lu samples [",i,col_id,col_sample.hash_);
+                } else {
+                    printf("table %zu str col %lu hash %lu samples [",i,col_id,col_sample.hash_);
+                }
+                for(uint64_t s:col_sample.samples_){
+                    printf("%lu, ",s);
+                }
+                printf("]\n");
+            }
+        }
+    }
+
     // 生成一个哈希表
     inline void generateCache(size_t node_id){
         cache_types_[node_id] = OWN_CACHE;
         hashmaps_[node_id] = new Hashmap();
     }
 
+    // 生成一个哈希表，但是这个哈希表不要保留为缓存
+    inline void generateTmpCache(size_t node_id){
+        cache_types_[node_id] = DONT_CACHE;
+        hashmaps_[node_id] = new Hashmap();
+    }
+
     // 使用哈希表缓存
     inline void setCache(size_t node_id, Hashmap* hashmap){
-        cache_types_[node_id] = HAS_CACHE;
+        cache_types_[node_id] = USE_CACHE;
         hashmaps_[node_id] = hashmap;
     }
 
+    // 无效化该节点上的缓存（如果有）
     inline void invalidCache(size_t node_id){
         cache_types_[node_id] = DONT_CACHE;
         if(hashmaps_[node_id]!= nullptr){
             delete hashmaps_[node_id];
         }
+    }
+
+    inline bool isNaiveJoin(size_t node_id){
+        if(!isJoinNode(node_id)){
+            return false;
+        }
+        const JoinNode &join = std::get<JoinNode>(nodes_[node_id].data);
+        size_t build_node = join.build_left ? join.left : join.right;
+        if(isJoinNode(build_node)){
+            return false;
+        }
+        const ScanNode &scan = std::get<ScanNode>(nodes_[build_node].data);
+        if(inputs_sample_[scan.base_table_id].num_rows_==1){
+            return true;
+        }
+        return false;
     }
 
     inline CacheType getCacheType(size_t node_id) const{
@@ -160,11 +222,24 @@ public:
         return hashes_[node_id];
     }
 
+    inline uint64_t getBuildSideHash(size_t node_id) const{
+        if(!isJoinNode(node_id)){
+            return INVALID_HASH;
+        }
+        const JoinNode &join = std::get<JoinNode>(nodes_[node_id].data);
+        size_t build_node = join.build_left ? join.left : join.right;
+        return hashes_[build_node];
+    }
+
     inline Hashmap* getHashmap(size_t node_id){
         return hashmaps_[node_id];
     }
 
-    // 判断两个query中的指定节点是否完全相同
+    inline bool isJoinNode(size_t node_id) const{
+        return std::holds_alternative<JoinNode>(nodes_[node_id].data);
+    }
+
+    // 判断两个query中的指定节点（的输出）是否完全相同
     bool isSame(size_t node_id, const QueryCache* other_query, size_t other_node_id) const{
         uint64_t this_hash = hashes_[node_id];
         uint64_t other_hash = other_query->hashes_[other_node_id];
@@ -206,14 +281,27 @@ public:
                 return false;
             }
             // 递归比较左右两侧的节点
-            if(isSame(this_join.left,other_query,other_join.left)){
+            if(!isSame(this_join.left,other_query,other_join.left)){
                 return false;
             }
-            if(isSame(this_join.right,other_query,other_join.right)){
+            if(!isSame(this_join.right,other_query,other_join.right)){
                 return false;
             }
         }
         return true;
+    }
+
+    // 判断两个query中的指定Join节点的构建侧是否完全相同
+    bool isBuildSideSame(size_t node_id, const QueryCache* other_query, size_t other_node_id) const{
+        if(!isJoinNode(node_id) || !other_query->isJoinNode(other_node_id)){
+            return false;
+        }
+        const JoinNode &join = std::get<JoinNode>(nodes_[node_id].data);
+        size_t build_node = join.build_left ? join.left : join.right;
+        const JoinNode &other_join = std::get<JoinNode>(other_query->nodes_[other_node_id].data);
+        size_t other_build_node = other_join.build_left ? other_join.left : other_join.right;
+
+        return isSame(build_node, other_query, other_build_node);
     }
 
     // 获取本query保存的哈希表的大小
@@ -237,18 +325,18 @@ private:
     std::vector<TableSample> inputs_sample_;
     size_t root;
 
-    static const uint64_t INVALID_HASH=1;   // 无效的哈希值
-    std::vector<uint64_t> hashes_;          // 每个节点的哈希值
+    inline static const uint64_t INVALID_HASH=1;   // 无效的哈希值
+    std::vector<uint64_t> hashes_;          // 每个节点（输出）的哈希值
 
     std::vector<CacheType> cache_types_;    // 每个节点是否需要缓存哈希表
-    std::vector<Hashmap*> hashmaps_;        // 每个节点（输出）的哈希表（可能是空的）
+    std::vector<Hashmap*> hashmaps_;        // 每个（join）节点（构建侧）的哈希表（可能是空的）
 
-    static const size_t MAX_NODE_DEPTH=2;   // 深度超过MAX_NODE_DEPTH的节点不缓存
+    static const size_t MAX_NODE_DEPTH=3;   // 深度超过MAX_NODE_DEPTH的节点不缓存
     std::vector<size_t> node_depth_;        // 每个节点的深度。scan节点深度为0，join节点深度为其构建侧深度+1
 
     // 对table_id的col_id列做采样并存储
     inline void addColumnSamples(size_t table_id, size_t col_id, const Column& column){
-        if(inputs_sample_[table_id].column_samples_.find(col_id) != inputs_sample_[table_id].column_samples_.end()){
+        if(inputs_sample_[table_id].column_samples_.find(col_id) == inputs_sample_[table_id].column_samples_.end()){
             inputs_sample_[table_id].column_samples_.emplace(col_id, ColumnSample(column, getTableSize(table_id)));
         }
     }
@@ -302,19 +390,18 @@ private:
                 node_depth_[node_id] = 0;
                 return scan_hash;
             } else if constexpr (std::is_same_v<T, JoinNode>) {
-                // 分获取左右算子的哈希值，如果任意一侧无效，则设置该节点为无效
+                // 分获取左右算子的哈希值，如果任意一侧无效，则设置该节点哈希值为无效
                 uint64_t left_hash = calculateNodeHash(plan,node_data.left);
                 uint64_t right_hash = calculateNodeHash(plan,node_data.right);
-                // 判断构建侧算子是否需要被被缓存
-                if(node_data.build_left && left_hash!=INVALID_HASH && node_depth_[node_data.left]<=MAX_NODE_DEPTH){
-                    cache_types_[node_data.left]=NEED_CACHE;
-                } else if(!node_data.build_left && right_hash!=INVALID_HASH && node_depth_[node_data.right]<=MAX_NODE_DEPTH){
-                    cache_types_[node_data.right]=NEED_CACHE;
+                node_depth_[node_id] = node_data.build_left ? node_depth_[node_data.left] : node_depth_[node_data.right];
+                node_depth_[node_id] ++;
+                // 判断本join的哈希表是否需要被被缓存
+                if(node_depth_[node_id] <= MAX_NODE_DEPTH &&
+                    ((node_data.build_left && left_hash!=INVALID_HASH) || (!node_data.build_left && right_hash!=INVALID_HASH))){
+                    cache_types_[node_id]=NEED_CACHE;
                 }
                 if(left_hash==INVALID_HASH || right_hash==INVALID_HASH){
                     hashes_[node_id] = INVALID_HASH;
-                    node_depth_[node_id] = node_data.build_left ? node_depth_[node_data.left] : node_depth_[node_data.right];
-                    node_depth_[node_id] ++;
                     return INVALID_HASH;
                 }
                 std::vector<uint64_t> datas;
@@ -330,8 +417,6 @@ private:
                 }
                 uint64_t join_hash = hash_range(datas.begin(),datas.end());
                 hashes_[node_id] = join_hash;
-                node_depth_[node_id] = node_data.build_left ? node_depth_[node_data.left] : node_depth_[node_data.right];
-                node_depth_[node_id] ++;
                 return join_hash;
             }
         }, node.data);
@@ -351,7 +436,7 @@ class CacheManager{
     // unordered_map 用于将节点哈希值映射到 list 中对应位置的迭代器，方便查找和移动操作。
     std::unordered_map<uint64_t, std::list<std::pair<size_t, size_t>>::iterator> caches_map_;
 
-    static const size_t MAX_CACHE_SIZE=1024*1024*20;    // 最多20G
+    inline static const size_t MAX_CACHE_SIZE = 1024ULL * 1024 * 1024 * 20;    // 缓存的最大字节数，最多20G
     size_t cache_size_{0};     // 记录了hashmap_caches_中保存的所有哈希表的总字节数目
 
 public:
@@ -361,24 +446,38 @@ public:
 
         // 检查query的每个join节点的哈希表是否可以缓存
         for(size_t i=0; i<plan.nodes.size(); i++){
+            // 这是为了naive join的适配
+            if(query->isNaiveJoin(i)){
+                query->invalidCache(i);
+                continue;
+            }
             if(query->getCacheType(i)==QueryCache::NEED_CACHE){
-                uint64_t node_hash = query->getNodeHash(i);
+                // 获取该join的构建侧节点的哈希值
+                uint64_t node_hash = query->getBuildSideHash(i);
                 if(caches_map_.find(node_hash)!=caches_map_.end()){
                     auto it = caches_map_.at(node_hash);
                     auto [query_id, node_id] = *it;
                     QueryCache* cached_query = queries_[query_id];
-                    if(query->isSame(i,cached_query,node_id)){
+                    if(query->isBuildSideSame(i,cached_query,node_id)){
+#ifdef CACHE_LOG
+                        printf("use cached hashmap: query %lu node %lu\n", query_id, node_id);
+#endif
                         hashmap_caches_.splice(hashmap_caches_.end(), hashmap_caches_, it);     // 将复用的哈希表移到末尾
                         query->setCache(i,cached_query->getHashmap(node_id));
-                    } else {
-                        query->generateCache(i);
+                        continue;
                     }
-                } else {
-                    query->generateCache(i);
                 }
+                // 没有缓存的情况下，为该节点生成一个哈希表
+                query->generateCache(i);
+            } else if(query->isJoinNode(i)){
+                // 就算不需要缓存，Join节点也需要生成哈希表
+                query->generateTmpCache(i);
             }
         }
 
+#ifdef CACHE_LOG
+        query->print();
+#endif
         return query;
     }
 
@@ -395,17 +494,29 @@ public:
             // 空间已满，需要释放一些缓存
             auto [query_id, node_id] = hashmap_caches_.front();
             hashmap_caches_.pop_front();
-            caches_map_.erase(queries_[query_id]->getNodeHash(node_id));
+            caches_map_.erase(queries_[query_id]->getBuildSideHash(node_id));
             assert(cache_size_ >= queries_[query_id]->getHashmap(node_id)->getTotalSize());
             cache_size_ -= queries_[query_id]->getHashmap(node_id)->getMemSize();
             queries_[query_id]->invalidCache(node_id);
+#ifdef CACHE_LOG
+            printf("free cached hashmap: query %lu node %lu\n", query_id, node_id);
+#endif
         }
 
-        // 将本查询中的OWN_CACHE的节点加入到缓存中
+        // 将本查询中的OWN_CACHE的节点加入到缓存中。清理不需要缓存的哈希表
         for(size_t i=0; i<query->getNodeNum(); i++){
-            if(query->getCacheType(i)==QueryCache::OWN_CACHE && query->getHashmap(i)!=nullptr){
-                hashmap_caches_.emplace_back(queries_.size()-1,i);
-                caches_map_.emplace(query->getNodeHash(i),--hashmap_caches_.end());
+            if(query->isJoinNode(i)){
+                QueryCache::CacheType type = query->getCacheType(i);
+                Hashmap *hashmap = query->getHashmap(i);
+                if(type==QueryCache::OWN_CACHE && hashmap!=nullptr){
+                    hashmap_caches_.emplace_back(queries_.size()-1,i);
+                    caches_map_.emplace(query->getBuildSideHash(i),--hashmap_caches_.end());
+#ifdef CACHE_LOG
+                    printf("add cached hashmap: query %lu node %lu\n", queries_.size()-1, i);
+#endif
+                } else if(type==QueryCache::DONT_CACHE){
+                    query->invalidCache(i);
+                }
             }
         }
     }
