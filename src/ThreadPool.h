bcbcf2661bdf48dd6c3ef8a173b86f006b0a605f
Patrick16262 <1626295293@qq.com>
2025-03-30 17:29:55 +0000

    添加更多日志

--- src/ThreadPool.h
+++ src/ThreadPool.h
@@ -3,6 +3,7 @@
 #include <atomic>
 #include <condition_variable>
 #include <cstddef>
+#include <cstdio>
 #include <functional>
 #include <mutex>
 #include <queue>
@@ -12,78 +13,15 @@
 #include <hardware.h>
 #include "MemoryPool.hpp"
 
-class SimpleThreadPool {
-public:
-    // 构造函数：初始化线程池并启动指定数量的线程
-    explicit SimpleThreadPool(size_t threadCount) : stop(false) {
-        for (size_t i = 0; i < threadCount; ++i) {
-            workers.emplace_back([this]() {
-              local_allocator.init(&global_mempool);
-                while (true) {
-                  std::function<void()> task;
-
-                  {
-                    std::unique_lock<std::mutex> lock(queueMutex);
-                    condition.wait(lock,
-                                   [this]() { return stop || !tasks.empty(); });
-
-                    if (stop && tasks.empty())
-                      return;
-
-                    task = std::move(tasks.front());
-                    tasks.pop();
-                  }
-
-                  task();
-                }
-            });
-        }
-    }
-
-    // 向线程池添加任务
-    template <typename F>
-    void
-    enqueue(F&& f) {
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-
-            if (stop)
-                throw std::runtime_error("Enqueue on stopped ThreadPool");
-
-            tasks.emplace([f = std::forward<F>(f)]() { f(); });
-        }
-
-        condition.notify_one();
-    }
-
-    // 析构函数：停止所有线程并等待它们完成
-    ~SimpleThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queueMutex);
-            stop = true;
-        }
-
-        condition.notify_all();
-
-        for (std::thread& worker : workers) worker.join();
-    }
-
-private:
-    std::vector<std::thread> workers;         // 工作线程
-    std::queue<std::function<void()>> tasks;  // 任务队列
-    std::mutex queueMutex;                    // 任务队列互斥锁
-    std::condition_variable condition;        // 条件变量
-    std::atomic<bool> stop;                   // 停止标志
-};
-
-
 template <size_t ThreadCount>
 class StaticThreadPool {
 public:
     StaticThreadPool() : stop_flag(false) {
+        printf("Creating threadPool with %zu threads\n", ThreadCount);
         for (size_t i = 0; i < ThreadCount; ++i) {
             workers[i] = std::thread([this, i] {
-              local_allocator.init(&global_mempool);
+                printf("Thread %zu started\n", i);
+                local_allocator.init(&global_mempool);
                 while (!stop_flag) {
                     std::function<void()> task;
                     {
@@ -91,34 +29,35 @@ public:
                         conditions[i].wait(lock, [this, i] { return stop_flag || tasks[i]; });
 
                         if (stop_flag && !tasks[i]) {
-                            return;
+                            break;
                         }
 
                         task = std::move(tasks[i]);
                         tasks[i] = nullptr;
                     }
 
+                    printf("got a task in thread %zu\n", i);
                     task();
                 }
+                printf("Thread %zu stop\n", i);
             });
-            pthread_t thread = workers[i].native_handle();
-            int ret = pthread_setname_np(pthread_self(),  "fast-worker");
-            if (ret != 0) {
-                throw std::runtime_error("failed to set thread name");
-            }
         }
+        printf("ThreadPool created successfully\n");
     }
 
     ~StaticThreadPool() {
+        printf("Destroying threadPool\n");
         stop_flag = true;
         for (auto& condition : conditions) {
             condition.notify_all();
         }
+        printf("Waiting for threads to finish\n");
         for (std::thread &worker : workers) {
             if (worker.joinable()) {
                 worker.join();
             }
         }
+        printf("ThreadPool destroyed successfully\n");
     }
 
     template <typename Func>
